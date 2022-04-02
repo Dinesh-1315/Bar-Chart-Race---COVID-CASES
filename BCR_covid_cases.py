5b8139fc2385eb42fa0edb4a7ea1049ab507e062
Dinesh <100015589+Dinesh-1315@users.noreply.github.com>
2022-04-03 00:46:23 +0530

    Update BCR_covid_cases.py
    code file changes for address and paths

--- BCR_covid_cases.py
+++ BCR_covid_cases.py
@@ -3,7 +3,7 @@ import pandas as pd
 import bar_chart_race as bcr
 
 #loading the dataset
-data = pd.read_csv(r"C:\Users\pc\Desktop\Project BCR\COVID-19_WHO.csv", encoding="latin")
+data = pd.read_csv(r"..............\WHO-COVID-19-global-data.csv", encoding="latin")           # insert the path
 print(data.head(5))
 #print(data.columns)
 #print(data.describe)
@@ -17,8 +17,8 @@ print(Date_reported,Country_name)
 data_dict = dict()
             
 for i in Country_name:
-    n = df["Cumulative_cases"][ df.Country == i]          # n is a list here
-    data_dict[i]= n.tolist()                              # list to dictionary conversion
+    n = df["Cumulative_cases"][ df.Country == i]                                                # n is a list here
+    data_dict[i]= n.tolist()                                                                    # list to dictionary conversion
 
 #converting the "data_dict" dictionary into a dataset
 df_final= pd.DataFrame(data_dict)
@@ -28,25 +28,20 @@ print(df_final.tail(5))
 df_final.insert(0,"Date",Date_reported,True)
 print(df_final.tail(5))
 #saving the dataset to the specific location
-#t.to_csv(r"C:\Users\pc\Desktop\updated_demo02.csv",index = False)
+#t.to_csv(r"...........................\covid cases.csv",index = False)                         # insert the path
 
 #setting "Date" as an index
 df_final.set_index("Date",inplace=True)
 #sorted dataframe
-df_sorted = df_final.sort_values(by= '16-03-2022',axis=1,ascending= False)
+df_sorted = df_final.sort_values(by= '16-03-2022',axis=1,ascending= False)                      # insert the last date till which you have the dataset &insert the path
 #saving the dataset to the specific location
-df_sorted.to_csv(r"C:\Users\pc\Desktop\Project BCR\extra files\total cases 0001.csv", index = True)
-#transpose 
-df_trans=df_sorted.transpose()
-df_trans.to_csv(r"C:\Users\pc\Desktop\Project BCR\extra files\trans total cases 0001.csv", index = True)
-df_final= df_sorted.iloc[:,1:100]
-#df_final= df_sorted.iloc[150:250,[1,22,2,33,14,5,75,120,155,45,96,112,147,26,35,8,9,19,11]]
+df_sorted.to_csv(r".........................................\total cases.csv", index = True)
 
 # lets have some animation now , save the output in "filename"
 bcr.bar_chart_race(df=df_sorted,
-                   filename = r"C:\Users\pc\Desktop\Project BCR\extra videos\video001.mp4",
+                   filename = r"..................\video01.mp4",                                # insert the path
                    figsize=(9,4),
-                   period_length=800,     #600   #12:11
+                   period_length=800,     
                    n_bars=12,
                    bar_size=.80,
                    dpi=144,
